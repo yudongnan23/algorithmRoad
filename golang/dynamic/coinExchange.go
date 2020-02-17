5c1946584b93dd29bdcc4a80e3869a568de97c11
dell <yu_lbj23@163.com>
2020-02-17 13:14:33 +0800

    动态规划----硬币兑换

--- golang/dynamic/coinExchange.go
+++ golang/dynamic/coinExchange.go
@@ -1,30 +1,29 @@
 /*
 	给定指定的硬币种类及需要的钱的总数，组合给定的硬币得到钱的总数，并使得硬币数最少。
- */
+*/
 
 package main
 
 import "fmt"
 
-func coinExchange(coinArray []int, moneny int)int{
+func coinExchange(coinArray []int, moneny int) int {
 	const MAXVALUE = 9999
 	length := len(coinArray)
-	f := []int{0}
-	var i, j int
-	for i = 1; i<= moneny; i++{
+	f := make([]int, moneny+1)
+	f[0] = 0
+	for i := 1; i <= moneny; i++ {
 		minNum := MAXVALUE
-		for j = 0; j < length; j++{
-			if i >= coinArray[j] && minNum > f[i - coinArray[j]] + 1{
-				minNum = f[i - coinArray[j]] + 1
+		for j := 0; j < length; j++ {
+			if i >= coinArray[j] && minNum > f[i-coinArray[j]]+1 {
+				minNum = f[i-coinArray[j]] + 1
 			}
 		}
-		f = append(f, minNum)
+		f[i] = minNum
 	}
 
-	if f[moneny] == MAXVALUE{
+	if f[moneny] == MAXVALUE {
 		return -1
 	}
-	fmt.Println(f)
 	return f[moneny]
 }
 
