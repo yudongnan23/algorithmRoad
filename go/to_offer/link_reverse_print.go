37710292b2cfc6060098363c8d5f8881a4c22b26
yudongnan <yudongnan@inke.cn>
2023-03-07 23:27:24 +0800

    从尾到头打印链表

---
+++ go/to_offer/link_reverse_print.go
@@ -0,0 +1,13 @@
+package to_offer
+
+func reversePrint(head *ListNode) []int {
+	values := make([]int, 10000)
+	p := head
+	i := 9999
+	for p != nil {
+		values[i] = p.Val
+		p = p.Next
+		i--
+	}
+	return values[i:]
+}
