b2ad3660ba4ee1b9217afcf3049b965050fcda44
yudongnan <yudongnan@inke.cn>
2023-09-13 21:11:36 +0800

    n~1中缺失的数字

--- go/to_offer/missing_number.go
+++ go/to_offer/missing_number.go
@@ -1,11 +1,9 @@
 package to_offer
 
 func missingNumber(nums []int) int {
-	var sum int
-	for i := 0; i < len(nums); i++ {
-		sum = sum + nums[i]
+	for i := 1; i < len(nums); i++ {
+		nums[i] = nums[i] + nums[i-1]
 	}
 
-	n := len(nums)
-	return ((0+n)*(n+1))/2 - sum
+	return ((len(nums) * (len(nums) + 1)) / 2) - nums[len(nums)-1]
 }
