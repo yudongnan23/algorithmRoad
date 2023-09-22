b7375ad8c42e4a06323fa297eba5fcca3bd834c9
yudongnan <yudongnan@inke.cn>
2023-09-22 22:16:40 +0800

    翻转二叉树

--- go/to_offer/mirror_tree.go
+++ go/to_offer/mirror_tree.go
@@ -2,24 +2,22 @@ package to_offer
 
 func mirrorTree(root *TreeNode) *TreeNode {
 	if root == nil {
-		return nil
+		return root
 	}
 
-	queue := []*TreeNode{root}
-	for len(queue) != 0 {
-		curNode := queue[0]
-		queue = queue[1:]
-
-		curNode.Left, curNode.Right = curNode.Right, curNode.Left
-
-		if curNode.Left != nil {
-			queue = append(queue, curNode.Left)
-		}
-
-		if curNode.Right != nil {
-			queue = append(queue, curNode.Right)
-		}
-	}
-
+	turnOver(root)
 	return root
+}
+
+func turnOver(root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	temp := root.Left
+	root.Left = root.Right
+	root.Right = temp
+
+	turnOver(root.Left)
+	turnOver(root.Right)
 }
