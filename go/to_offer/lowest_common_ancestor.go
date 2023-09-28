7fb19e1e4f3e33686d9855a86cfb2a41bf15f753
yudongnan <yudongnan@inke.cn>
2023-09-28 22:39:28 +0800

    二叉搜索树的最近公共祖先

---
+++ go/to_offer/lowest_common_ancestor.go
@@ -0,0 +1,38 @@
+package to_offer
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	maxNode, minNode := sortNodeII(p, q)
+
+	return backTraceing(root, maxNode, minNode)
+}
+
+func backTraceing(root, maxNode, minNode *TreeNode) *TreeNode {
+	if root.Val < maxNode.Val && root.Val > minNode.Val {
+		return root
+	}
+
+	if root.Val == minNode.Val {
+		return minNode
+	}
+
+	if root.Val == maxNode.Val {
+		return maxNode
+	}
+
+	if root.Val < minNode.Val {
+		return lowestCommonAncestor(root.Right, minNode, maxNode)
+	}
+
+	return lowestCommonAncestor(root.Left, minNode, maxNode)
+}
+
+func sortNodeII(p, q *TreeNode) (*TreeNode, *TreeNode) {
+	if p.Val > q.Val {
+		return p, q
+	}
+	return q, p
+}
