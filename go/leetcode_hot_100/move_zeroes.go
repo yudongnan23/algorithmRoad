12e01c6e60d19b2139a9a7eb76086da18b84a310
yudongnan <yudongnan@inke.cn>
2023-10-25 22:56:49 +0800

    移动零

---
+++ go/leetcode_hot_100/move_zeroes.go
@@ -0,0 +1,19 @@
+package leetcode_hot_100
+
+func moveZeroes(nums []int) {
+	left := 0
+	right := 0
+
+	for right < len(nums) {
+		if nums[left] == 0 && nums[right] != 0 {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+		}
+
+		if nums[left] != 0 {
+			left++
+		}
+
+		right++
+	}
+}
