ef44ecb807fb812fed1447af79169440b58bf6f2
yudongnan <yudongnan@inke.cn>
2023-10-28 16:59:58 +0800

    盛最多水的容器

---
+++ go/leetcode_hot_100/max_area.go
@@ -0,0 +1,30 @@
+package leetcode_hot_100
+
+func maxArea(height []int) int {
+	left := 0
+	right := len(height) - 1
+	max := 0
+
+	for left < right {
+		curArea := (right - left) * min(height[left], height[right])
+		if curArea > max {
+			max = curArea
+		}
+
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return max
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+
+	return j
+}
