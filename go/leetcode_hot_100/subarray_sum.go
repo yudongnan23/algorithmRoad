059cfcaf518711500707e3f7dcabb90e8c5b29db
yudongnan <yudongnan@inke.cn>
2023-11-06 22:32:50 +0800

    和为 K 的子数组

---
+++ go/leetcode_hot_100/subarray_sum.go
@@ -0,0 +1,16 @@
+package leetcode_hot_100
+
+// TODO again
+func subarraySum(nums []int, k int) int {
+	m := map[int]int{0: 1}
+	var sum int
+	var count int
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+		if v, ok := m[sum-k]; ok {
+			count += v
+		}
+		m[sum]++
+	}
+	return count
+}
