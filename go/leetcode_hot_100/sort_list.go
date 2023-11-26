e64922f2bd1a9bf46fce598ff99c849e9dde98e5
yudongnan <yudongnan@inke.cn>
2023-11-26 16:56:21 +0800

    排序链表

---
+++ go/leetcode_hot_100/sort_list.go
@@ -0,0 +1,27 @@
+package leetcode_hot_100
+
+// TODO again
+func sortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	mid := findMid(head)
+
+	left := sortList(head)
+	right := sortList(mid)
+
+	return mergeTwoLists(left, right)
+}
+
+func findMid(head *ListNode) *ListNode {
+	fast := head.Next
+	slow := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	mid := slow.Next
+	slow.Next = nil
+	return mid
+}
