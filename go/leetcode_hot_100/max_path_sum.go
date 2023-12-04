8d77225998593e7b5c15ca8ecdbedf29c05f55f0
yudongnan <yudongnan@inke.cn>
2023-12-04 23:36:46 +0800

    二叉树中的最大路径和

---
+++ go/leetcode_hot_100/max_path_sum.go
@@ -0,0 +1,38 @@
+package leetcode_hot_100
+
+const (
+	minNum = -(2 << 32)
+)
+
+// TODO again
+func maxPathSum(root *TreeNode) int {
+	maxLength := -(2 << 32)
+	maxLen := dfsIIII(root, &maxLength)
+	if maxLen > maxLength {
+		maxLength = maxLen
+	}
+	return maxLength
+}
+
+func dfsIIII(root *TreeNode, maxLength *int) int {
+	if root == nil {
+		return minNum
+	}
+
+	leftMaxLength := dfsIIII(root.Left, maxLength)
+	rightMaxLength := dfsIIII(root.Right, maxLength)
+
+	curMaxLength := maxInTwo(maxInTwo(leftMaxLength+rightMaxLength+root.Val, leftMaxLength), rightMaxLength)
+	if curMaxLength > *maxLength {
+		*maxLength = curMaxLength
+	}
+
+	return maxInTwo(maxInTwo(rightMaxLength+root.Val, leftMaxLength+root.Val), root.Val)
+}
+
+func maxInTwo(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
