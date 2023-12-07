7da016f34bd22aca91bf1d84988d65517bbb2a92
yudongnan <yudongnan@inke.cn>
2023-12-07 23:50:24 +0800

    子集

---
+++ go/leetcode_hot_100/subsets.go
@@ -0,0 +1,17 @@
+package leetcode_hot_100
+
+import "math"
+
+// TODO again
+func subsets(nums []int) [][]int {
+	ans := make([][]int, 1, int(math.Pow(2, float64(len(nums))))+1)
+	ans[0] = []int{}
+	for _, x := range nums {
+		for _, arr := range ans {
+			a := make([]int, len(arr), len(arr)+1)
+			copy(a, arr)
+			ans = append(ans, append(a, x))
+		}
+	}
+	return ans
+}
