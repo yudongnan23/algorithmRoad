df9d6297aa45b30f5e3080e14accfa63de684027
yudongnan <yudongnan@inke.cn>
2023-12-24 14:50:29 +0800

    跳跃游戏

---
+++ go/leetcode_hot_100/can_jump.go
@@ -0,0 +1,13 @@
+package leetcode_hot_100
+
+func canJump(nums []int) bool {
+	i, right, n := 0, nums[0], len(nums)-1
+	for i <= right && i <= n {
+		curRight := i + nums[i]
+		if right < curRight {
+			right = curRight
+		}
+		i++
+	}
+	return right >= n
+}
