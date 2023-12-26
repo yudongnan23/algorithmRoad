e58202c4b63cff606112807b932c5e9cebcda744
yudongnan <yudongnan@inke.cn>
2023-12-26 23:35:23 +0800

    完全平方数

---
+++ go/leetcode_hot_100/num_squares.go
@@ -0,0 +1,26 @@
+package leetcode_hot_100
+
+var squareNums = make([]int, 0)
+var dp = []int{0}
+
+// TODO again
+func numSquares(n int) int {
+	if n < len(dp) {
+		return dp[n]
+	}
+	for i := len(squareNums) + 1; i*i <= n; i++ {
+		squareNums = append(squareNums, i*i)
+	}
+	for i := len(dp); i <= n; i++ {
+		dp = append(dp, i)
+		for _, square := range squareNums {
+			if i < square {
+				break
+			}
+			if dp[i-square]+1 < dp[i] {
+				dp[i] = dp[i-square] + 1
+			}
+		}
+	}
+	return dp[n]
+}
