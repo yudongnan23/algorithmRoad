b89104ea6cce9466fb3734232b5f658b7225e6e6
yudongnan <yudongnan@inke.cn>
2023-12-29 23:18:25 +0800

    分割等和子集

--- go/leetcode_hot_100/can_partition.go
+++ go/leetcode_hot_100/can_partition.go
@@ -4,19 +4,31 @@ package leetcode_hot_100
 func canPartition(nums []int) bool {
 	sum := 0
 	for _, v := range nums {
-		sum += v
+		sum = sum + v
 	}
-	target := sum / 2
-	if target*2 != sum {
+
+	halfSum := sum / 2
+
+	if sum%2 != 0 {
 		return false
 	}
-	dp := make([]int, target+1)
 
+	dp := make([]int, halfSum+1)
 	for i := 0; i < len(nums); i++ {
-		for j := target; j >= nums[i]; j-- {
-			dp[j] = max(dp[j], dp[j-nums[i]]+nums[i])
+		if nums[i] == halfSum {
+			return true
+		}
+		for j := halfSum; j >= nums[i]; j-- {
+			curNum := dp[j-nums[i]] + nums[i]
+			if curNum == halfSum {
+				return true
+			}
+
+			if curNum > dp[j] {
+				dp[j] = curNum
+			}
 		}
 	}
-	// fmt.Println(dp)
-	return dp[target] == target
+
+	return false
 }
