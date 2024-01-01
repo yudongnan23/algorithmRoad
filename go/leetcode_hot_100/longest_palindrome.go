b2784f2155c07c6d6ff68eff551ec7a72ebd7fd9
yudongnan <yudongnan@inke.cn>
2024-01-01 23:01:19 +0800

    最长回文子串

---
+++ go/leetcode_hot_100/longest_palindrome.go
@@ -0,0 +1,24 @@
+package leetcode_hot_100
+
+func longestPalindrome(s string) string {
+	n := len(s)
+	dp := make([][]bool, n)
+	for i, _ := range dp {
+		dp[i] = make([]bool, n)
+		dp[i][i] = true
+	}
+	ans := ""
+	for r := 0; r < n; r++ {
+		for l := 0; l <= r; l++ {
+			if r-l == 1 {
+				dp[l][r] = s[l] == s[r]
+			} else if r > l {
+				dp[l][r] = (s[l] == s[r]) && dp[l+1][r-1]
+			}
+			if dp[l][r] && r-l+1 > len(ans) {
+				ans = s[l : r+1]
+			}
+		}
+	}
+	return ans
+}
