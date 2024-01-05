0d934c19dc64f72f77ee0fd91b3e6a05b2f0524b
yudongnan <yudongnan@inke.cn>
2024-01-05 23:33:24 +0800

    找到字符串中所有字母异位词

--- go/leetcode_hot_100/find_anagrams.go
+++ go/leetcode_hot_100/find_anagrams.go
@@ -1,95 +1,40 @@
 package leetcode_hot_100
 
-// TODO Again
+// TODO three
 func findAnagrams(s string, p string) []int {
-	pLen := len(p)
-	res := make([]int, 0)
-
-	if pLen > len(s) {
-		return res
+	targetSize := len(p)
+	pCount := [26]int{}
+	for i := 0; i < targetSize; i++ {
+		pCount[int(p[i])-97]++
 	}
 
-	newP := sortWords(p)
-	subStr := sortWords(s[:pLen])
-
-	if subStr == newP {
-		res = append(res, 0)
-	}
-
-	n := len(s) - pLen + 1
-
-	for i := 1; i < n; i++ {
-		subStr = insertSort(subStr, s[i+pLen-1], s[i-1])
-		if subStr == newP {
-			res = append(res, i)
+	size := len(s)
+	l := 0
+	r := 0
+	res := make([]int, 0, size)
+	slideCount := [26]int{}
+	for r < size {
+		slideCount[int(s[r])-97]++
+		if r-l < targetSize-1 {
+			r++
+			continue
 		}
-	}
 
+		if isAnagram(slideCount, pCount) {
+			res = append(res, l)
+		}
+		slideCount[int(s[l])-97]--
+		r++
+		l++
+	}
 	return res
 }
 
-func insertSort(src string, newByte, deleteByte byte) string {
-	if newByte == deleteByte {
-		return src
-	}
-
-	searchFunc := func(bs []byte, dst byte) int {
-		left := 0
-		right := len(bs) - 1
-		for left <= right {
-			mid := (left + right) / 2
-			if bs[mid] == dst {
-				left = mid
-				break
-			}
-
-			if bs[mid] > dst {
-				right = mid - 1
-				continue
-			}
-
-			if bs[mid] < dst {
-				left = mid + 1
-				continue
-			}
+func isAnagram(m1, m2 [26]int) bool {
+	for i := 0; i < len(m1); i++ {
+		if m1[i] != m2[i] {
+			return false
 		}
-
-		if left >= len(bs) {
-			return len(bs) - 1
-		}
-
-		return left
 	}
-
-	// 删除
-	bs := []byte(src)
-	deleteIndex := searchFunc(bs, deleteByte)
-	newBs := bs[0:deleteIndex]
-	newBs = append(newBs, bs[deleteIndex+1:]...)
-
-	// 新增
-	b := make([]byte, len(newBs)+1)
-	if len(newBs) == 0 {
-		b[0] = newByte
-		return string(b)
-	}
-
-	if newByte <= newBs[0] {
-		b[0] = newByte
-		copy(b[1:], newBs)
-		return string(b)
-	}
-
-	if newByte >= newBs[len(newBs)-1] {
-		b[len(b)-1] = newByte
-		copy(b[:len(b)-1], newBs)
-		return string(b)
-	}
-
-	newIndex := searchFunc(newBs, newByte)
-	copy(b[:newIndex], newBs[:newIndex])
-	b[newIndex] = newByte
-	copy(b[newIndex+1:], newBs[newIndex:])
-
-	return string(b)
+	return true
 }
