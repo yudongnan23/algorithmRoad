922eae9899d5ead2ef92b5bdddef5804f0401223
yudongnan <yudongnan@inke.cn>
2024-01-07 23:17:07 +0800

    缺失的第一个正数

--- go/leetcode_hot_100/first_missing_positive.go
+++ go/leetcode_hot_100/first_missing_positive.go
@@ -1,29 +1,20 @@
 package leetcode_hot_100
 
-import "sort"
-
-// TODO again
+// TODO three
 func firstMissingPositive(nums []int) int {
-	sort.Slice(nums, func(i, j int) bool {
-		if nums[i] < nums[j] {
-			return true
+	size := len(nums)
+	for i := 0; i < size; {
+		if nums[i] >= 1 && nums[i] <= size && nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+		} else {
+			i++
 		}
-		return false
-	})
-
-	length := len(nums)
-	min := 1
-	for i := 0; i < length; i++ {
-		if nums[i] <= 0 {
-			continue
-		}
-		if i > 0 && nums[i-1] == nums[i] {
-			continue
-		}
-		if min != nums[i] {
-			break
-		}
-		min++
 	}
-	return min
+
+	for i := 0; i < size; i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+	return size + 1
 }
