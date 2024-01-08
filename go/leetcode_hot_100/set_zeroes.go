c1135eb01c9eabd54cc7cf994d907194c5ed178d
yudongnan <yudongnan@inke.cn>
2024-01-08 21:32:48 +0800

    矩阵置零

--- go/leetcode_hot_100/set_zeroes.go
+++ go/leetcode_hot_100/set_zeroes.go
@@ -1,43 +1,37 @@
 package leetcode_hot_100
 
 func setZeroes(matrix [][]int) {
-	zeroMap := make(map[int]bool, len(matrix)*len(matrix[0]))
-	row := make(map[int]bool, len(matrix))
-	column := make(map[int]bool, len(matrix[0]))
+	locMap := make(map[int]bool, 0)
+	m := len(matrix)
+	n := len(matrix[0])
 
-	changeFunc := func(i, j int) {
-		if !row[i] {
-			for m := 0; m < len(matrix[i]); m++ {
-				if matrix[i][m] == 0 {
-					continue
-				}
-				zeroMap[makeKey(i, m)] = true
-				matrix[i][m] = 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == 0 {
+				locMap[makeKey(i, j)] = true
 			}
-			row[i] = true
-		}
-
-		if !column[j] {
-			for n := 0; n < len(matrix); n++ {
-				if matrix[n][j] == 0 {
-					continue
-				}
-				zeroMap[makeKey(n, j)] = true
-				matrix[n][j] = 0
-			}
-			column[j] = true
 		}
 	}
 
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[i]); j++ {
-			if !zeroMap[makeKey(i, j)] && matrix[i][j] == 0 {
-				changeFunc(i, j)
+	markFunc := func(i, j int) {
+		for k := 0; k < n; k++ {
+			matrix[i][k] = 0
+		}
+
+		for l := 0; l < m; l++ {
+			matrix[l][j] = 0
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if locMap[makeKey(i, j)] {
+				markFunc(i, j)
 			}
 		}
 	}
 }
 
 func makeKey(i, j int) int {
-	return i<<32 | j
+	return (i << 32) | j
 }
