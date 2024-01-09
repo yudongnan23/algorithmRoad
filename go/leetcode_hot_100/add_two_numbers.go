9aebe185eeb6dc24c32640cbd7a6fcbd69fef4f7
yudongnan <yudongnan@inke.cn>
2024-01-09 22:48:53 +0800

    两数相加

--- go/leetcode_hot_100/add_two_numbers.go
+++ go/leetcode_hot_100/add_two_numbers.go
@@ -1,56 +1,37 @@
 package leetcode_hot_100
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var newHead *ListNode
-	p := newHead
-
-	flag := false
-
-	createNewNode := func(a, b *ListNode) *ListNode {
-		var val int
-		if a != nil {
-			val = a.Val
-		}
-		if b != nil {
-			val = val + b.Val
-		}
-		if flag {
-			val++
-			flag = false
-		}
-		if val >= 10 {
-			flag = true
-			val = val - 10
-		}
-
-		return &ListNode{
-			Val: val,
-		}
-	}
+	isCarry := false
+	head := &ListNode{}
+	p := head
 
 	for l1 != nil || l2 != nil {
-		newNode := createNewNode(l1, l2)
-		if newHead == nil {
-			newHead = newNode
-			p = newNode
-		} else {
-			p.Next = newNode
-			p = p.Next
-		}
-
+		val := 0
 		if l1 != nil {
+			val = l1.Val
 			l1 = l1.Next
 		}
 		if l2 != nil {
+			val = val + l2.Val
 			l2 = l2.Next
 		}
-	}
-
-	if flag {
-		p.Next = &ListNode{
-			Val: 1,
+		if isCarry {
+			isCarry = false
+			val++
 		}
+
+		if val >= 10 {
+			val = val - 10
+			isCarry = true
+		}
+
+		p.Next = &ListNode{Val: val}
+		p = p.Next
 	}
 
-	return newHead
+	if isCarry {
+		p.Next = &ListNode{Val: 1}
+	}
+
+	return head.Next
 }
