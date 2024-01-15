41295a221ca6894310eb6aa89b8f7df143b3c2da
yudongnan <yudongnan@inke.cn>
2024-01-15 21:54:05 +0800

    岛屿数量

--- go/leetcode_hot_100/num_islands.go
+++ go/leetcode_hot_100/num_islands.go
@@ -1,72 +1,55 @@
 package leetcode_hot_100
 
-const (
-	zero byte = 48
-	one  byte = 49
-)
-
-// TODO again
 func numIslands(grid [][]byte) int {
 	if len(grid) == 0 || len(grid[0]) == 0 {
 		return 0
 	}
 
+	n := len(grid)
+	m := len(grid[0])
 	count := 0
+	visited := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		visited[i] = make([]bool, m)
+	}
 
-	for i := 0; i < len(grid); i++ {
-		for j := 0; j < len(grid[i]); j++ {
-			if grid[i][j] == zero {
+	var visit func(i, j int)
+
+	visit = func(i, j int) {
+		visited[i][j] = true
+		// 上
+		if i > 0 && grid[i-1][j] == '1' && !visited[i-1][j] {
+			visit(i-1, j)
+		}
+
+		// 下
+		if i < n-1 && grid[i+1][j] == '1' && !visited[i+1][j] {
+			visit(i+1, j)
+		}
+
+		// 左
+		if j > 0 && grid[i][j-1] == '1' && !visited[i][j-1] {
+			visit(i, j-1)
+		}
+
+		// 右
+		if j < m-1 && grid[i][j+1] == '1' && !visited[i][j+1] {
+			visit(i, j+1)
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if grid[i][j] == '0' {
 				continue
 			}
 
-			count++
-
-			queue := make([]int, 1)
-			queue[0] = makeKey(i, j)
-			grid[i][j] = zero
-
-			for len(queue) != 0 {
-				cur := queue[0]
-				queue = queue[1:]
-
-				m, n := parseKey(cur)
-
-				// 上
-				if m > 0 && grid[m-1][n] == one {
-					grid[m-1][n] = zero
-					queue = append(queue, makeKey(m-1, n))
-				}
-
-				// 下
-				if m < len(grid)-1 && grid[m+1][n] == one {
-					grid[m+1][n] = zero
-					queue = append(queue, makeKey(m+1, n))
-				}
-
-				// 左
-				if n > 0 && grid[m][n-1] == one {
-					grid[m][n-1] = zero
-					queue = append(queue, makeKey(m, n-1))
-				}
-
-				// 右
-				if n < len(grid[i])-1 && grid[m][n+1] == one {
-					grid[m][n+1] = zero
-					queue = append(queue, makeKeyII(m, n+1))
-				}
+			if !visited[i][j] {
+				visit(i, j)
+				count++
 			}
 		}
 	}
 
 	return count
-}
-
-func makeKeyII(i, j int) int {
-	return i<<16 | j
-}
-
-func parseKey(n int) (int, int) {
-	i := n >> 16
-	j := ((1 << 16) - 1) & n
-	return i, j
 }
