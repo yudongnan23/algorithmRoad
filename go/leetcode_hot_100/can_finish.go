b96301e2e8061a5e41f07f3068fbd67094cbc716
yudongnan <yudongnan@inke.cn>
2024-01-16 21:43:16 +0800

    课程表

--- go/leetcode_hot_100/can_finish.go
+++ go/leetcode_hot_100/can_finish.go
@@ -1,17 +1,14 @@
 package leetcode_hot_100
 
-// TODO again
+// TODO three
 func canFinish(numCourses int, prerequisites [][]int) bool {
-	grap := make([][]int, numCourses)
+	graph := make([][]int, numCourses)
 	inDeg := make([]int, numCourses)
-
-	// 先将图构建好，并统计七每个节点的入度
 	for i := 0; i < len(prerequisites); i++ {
-		grap[prerequisites[i][1]] = append(grap[prerequisites[i][1]], prerequisites[i][0])
+		graph[prerequisites[i][1]] = append(graph[prerequisites[i][1]], prerequisites[i][0])
 		inDeg[prerequisites[i][0]]++
 	}
 
-	// 找出入度为0的节点
 	queue := make([]int, 0)
 	for i := 0; i < len(inDeg); i++ {
 		if inDeg[i] == 0 {
@@ -19,17 +16,16 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 		}
 	}
 
-	// 每次都从入度为0的节点开始，将其指向的节点，挨个拆除
 	courses := 0
 	for len(queue) > 0 {
 		curCourse := queue[0]
 		queue = queue[1:]
 		courses++
 
-		for i := 0; i < len(grap[curCourse]); i++ {
-			inDeg[grap[curCourse][i]]--
-			if inDeg[grap[curCourse][i]] == 0 {
-				queue = append(queue, grap[curCourse][i])
+		for i := 0; i < len(graph[curCourse]); i++ {
+			inDeg[graph[curCourse][i]]--
+			if inDeg[graph[curCourse][i]] == 0 {
+				queue = append(queue, graph[curCourse][i])
 			}
 		}
 	}
