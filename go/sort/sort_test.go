07f10609e80f6ef9ffb99c8c5d6f7c7f9dbed19d
yudongnan <yudongnan@inke.cn>
2024-02-07 14:58:40 +0800

    冒泡排序

--- go/sort/sort_test.go
+++ go/sort/sort_test.go
@@ -12,7 +12,13 @@ func Test_bubbleSort(t *testing.T) {
 }
 
 func Test_selectSort(t *testing.T) {
-	nums := []int{1, 2, 3, 4, 8, 6, 5, 7, 10, 9}
+	nums := []int{9, 10, 1, 2, 4, 3, 7, 8, 6, 5}
 	selectSort(nums)
+	fmt.Println(nums)
+}
+
+func Test_insertSort(t *testing.T) {
+	nums := []int{9, 10, 1, 2, 4, 3, 7, 8, 6, 5}
+	insertSort(nums)
 	fmt.Println(nums)
 }
