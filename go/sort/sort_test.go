f3c45aa19378a4373bc9bdbe320caf28cc3e7adb
yudongnan <yudongnan@inke.cn>
2024-02-18 14:44:22 +0800

    快速排序

--- go/sort/sort_test.go
+++ go/sort/sort_test.go
@@ -28,3 +28,9 @@ func Test_shellSort(t *testing.T) {
 	shellSort(nums)
 	fmt.Println(nums)
 }
+
+func Test_quickSort(t *testing.T) {
+	nums := []int{9, 10, 1, 2, 4, 3, 7, 8, 6, 5}
+	quickSort(nums)
+	fmt.Println(nums)
+}
