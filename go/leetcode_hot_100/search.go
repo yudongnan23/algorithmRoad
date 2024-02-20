0719e305ba84be915b98dcbbe9f86c134fbebd87
yudongnan <yudongnan@inke.cn>
2024-02-20 22:23:20 +0800

    搜索旋转排序数组

--- go/leetcode_hot_100/search.go
+++ go/leetcode_hot_100/search.go
@@ -1,63 +1,46 @@
 package leetcode_hot_100
 
-// TODO again
+// TODO three
 func searchII(nums []int, target int) int {
-	if len(nums) == 0 {
-		return -1
-	}
-
-	var dfs func(int, int) int
-	dfs = func(left, right int) int {
-		if left > right {
-			return -1
-		}
-
-		if right-left == 0 && nums[left] != target {
-			return -1
-		}
-
-		if right-left == 0 && nums[left] == target {
-			return left
-		}
-
-		if nums[left] <= nums[right] {
-			index := twoWay(nums[left:right+1], target)
-			if index != -1 {
-				return index + left
-			}
-		}
-
-		mid := (left + right) / 2
-		leftIndex := dfs(left, mid)
-		if leftIndex != -1 {
-			return leftIndex
-		}
-
-		return dfs(mid+1, right)
-	}
-
-	return dfs(0, len(nums)-1)
+	return dfsSearchII(nums, 0, len(nums)-1, target)
 }
 
-func twoWay(nums []int, target int) int {
+func dfsSearchII(nums []int, left, right, target int) int {
+	if left > right {
+		return -1
+	}
+	if nums[left] <= nums[right] {
+		return searchInNums(nums[left:right+1], left, target)
+	}
+	mid := (left + right) / 2
+	leftIndex := dfsSearchII(nums, left, mid, target)
+	if leftIndex > -1 {
+		return leftIndex
+	}
+	rightIndex := dfsSearchII(nums, mid+1, right, target)
+	if rightIndex > -1 {
+		return rightIndex
+	}
+	return -1
+}
+
+func searchInNums(nums []int, addIndex, target int) int {
 	left := 0
 	right := len(nums) - 1
 
 	for left <= right {
 		mid := (left + right) / 2
-
 		if nums[mid] == target {
-			return mid
+			return mid + addIndex
 		}
 
 		if nums[mid] > target {
-			left = mid + 1
+			right = mid - 1
 		}
 
 		if nums[mid] < target {
-			right = mid - 1
+			left = mid + 1
 		}
 	}
-
 	return -1
 }
